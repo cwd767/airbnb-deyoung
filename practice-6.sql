b42d63f7d741c9efa8f7e2be392af58b26da61c9
Creighton <creighton.deyoung@kellogg.northwestern.edu>
2026-01-21 18:13:39 +0000

    got stuck on how to connect data between tables - figured it out after struggling with a couple iterations, then Claude teaching me

--- practice-6.sql
+++ practice-6.sql
@@ -16,3 +16,9 @@
 -- | Burnside               | 10       |
 
 
+SELECT listings.neighborhood, COUNT(listing_id)
+FROM reviews
+INNER JOIN listings ON reviews.listing_id = listings.id
+GROUP BY neighborhood
+ORDER BY neighborhood
+;
