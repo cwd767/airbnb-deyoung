012e1dce6b7655b1697e85fada64c9b18396e4bd
Creighton <creighton.deyoung@kellogg.northwestern.edu>
2026-01-21 17:29:01 +0000

    lost progress, restarting. Practice 1 attemt 1

--- practice-1.sql
+++ practice-1.sql
@@ -4,4 +4,18 @@
 -- | 302750   |
 -- +----------+
 
+-- SELECT ____, ____ 
+-- FROM ____ 
+-- INNER JOIN ____ 
+-- ON ____ = ____ 
+-- WHERE ____ (AND ____) 
+-- GROUP BY ____ 
+-- ORDER BY ____ (DESC) 
+-- LIMIT ____;
+
+SELECT listing_id
+FROM reviews
+ORDER BY listing_id DESC
+LIMIT 1
+;
 
